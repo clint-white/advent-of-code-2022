978fdbba84a4554ccd0633900aa7513afaeda613
Clint White <104277906+clint-white@users.noreply.github.com>
2022-12-05 20:06:02 -0500

    day 5: Use `Display` trait for printing errors

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,10 +1,21 @@
 //! Advent of Code 2022 Day 5
 
+use std::env;
 use std::io;
 
-use day05::{parse_input, solve_part1, solve_part2, Result};
+use day05::{parse_input, solve_part1, solve_part2, Error, Result};
 
-fn main() -> Result<()> {
+fn main() {
+    match try_main() {
+        Ok(()) => (),
+        Err(Error::Io(error)) if error.kind() == io::ErrorKind::BrokenPipe => (),
+        Err(error) => {
+            eprintln!("{}: {error}", env!("CARGO_BIN_NAME"));
+        }
+    }
+}
+
+fn try_main() -> Result<()> {
     let input = io::read_to_string(io::stdin())?;
     let (mut stacks, instructions) = parse_input(&input)?;
 
