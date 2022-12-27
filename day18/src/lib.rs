529518e95ebf1b771b1c9ee08c93d4a7f4392127
Clint White <104277906+clint-white@users.noreply.github.com>
2022-12-27 17:05:10 -0500

    Day 18: Refactor solution to make more use of `ndarray`
    This version uses more features of the `ndarray` crate in an effort to
    reduce code duplication.  By using the views, slices, and iterators it
    provides, we can largely eliminate the use of indices to access array
    elements which often leads to highly repetitive code.
    
    This version also refactors the function which counts boundary faces to
    use arrays instead of hash maps.  We still use a hash set as a way to
    dedupe the list of cubes, as the counting method will fail if the list
    contains the a cube more than once.

--- day18/src/lib.rs
+++ day18/src/lib.rs
@@ -1,12 +1,12 @@
 //! Solutions to [Advent of Code 2022 Day 18](https://adventofcode.com/2022/day/18).
 
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 use std::hash::Hash;
 use std::io;
 use std::num::ParseIntError;
 use std::str::FromStr;
 
-use ndarray::{Array3, ArrayViewMut3};
+use ndarray::{s, Array3, ArrayViewMut1, ArrayViewMut3, Axis};
 use thiserror::Error;
 
 /// The error type for operations in this crate.
@@ -84,47 +84,42 @@ where
     s.lines().map(str::parse).collect()
 }
 
-type Face<T> = (T, T, T);
+/// Returns the number of cube faces which are not shared with another cube.
+pub fn count_boundary_faces(cubes: &[Cube<usize>]) -> usize {
+    // Remove any duplicate cubes so that we can assert that any face belongs to at most two cubes
+    // in the sample.
+    let cubes: HashSet<_> = cubes.iter().copied().collect();
 
-fn boundary_faces<I>(cubes: I) -> (HashSet<Face<u8>>, HashSet<Face<u8>>, HashSet<Face<u8>>)
-where
-    I: IntoIterator<Item = Cube<u8>>,
-{
-    let mut faces_yz = HashMap::new();
-    let mut faces_xz = HashMap::new();
-    let mut faces_xy = HashMap::new();
-    for cube in cubes {
-        *faces_yz.entry((cube.x, cube.y, cube.z)).or_insert(0) += 1;
-        *faces_yz.entry((cube.x + 1, cube.y, cube.z)).or_insert(0) += 1;
-        *faces_xz.entry((cube.x, cube.y, cube.z)).or_insert(0) += 1;
-        *faces_xz.entry((cube.x, cube.y + 1, cube.z)).or_insert(0) += 1;
-        *faces_xy.entry((cube.x, cube.y, cube.z)).or_insert(0) += 1;
-        *faces_xy.entry((cube.x, cube.y, cube.z + 1)).or_insert(0) += 1;
+    let xlimit = 2 + cubes.iter().map(|cube| cube.x).max().unwrap_or(0);
+    let ylimit = 2 + cubes.iter().map(|cube| cube.y).max().unwrap_or(0);
+    let zlimit = 2 + cubes.iter().map(|cube| cube.z).max().unwrap_or(0);
+
+    let mut faces_yz: Array3<usize> = Array3::default((xlimit, ylimit, zlimit));
+    let mut faces_xz: Array3<usize> = Array3::default((xlimit, ylimit, zlimit));
+    let mut faces_xy: Array3<usize> = Array3::default((xlimit, ylimit, zlimit));
+
+    for cube in &cubes {
+        faces_yz[[cube.x, cube.y, cube.z]] += 1;
+        faces_yz[[cube.x + 1, cube.y, cube.z]] += 1;
+        faces_xz[[cube.x, cube.y, cube.z]] += 1;
+        faces_xz[[cube.x, cube.y + 1, cube.z]] += 1;
+        faces_xy[[cube.x, cube.y, cube.z]] += 1;
+        faces_xy[[cube.x, cube.y, cube.z + 1]] += 1;
     }
-    assert!(faces_yz.values().all(|&count| count == 1 || count == 2));
-    assert!(faces_xz.values().all(|&count| count == 1 || count == 2));
-    assert!(faces_xy.values().all(|&count| count == 1 || count == 2));
-    let boundary_yz = faces_yz
-        .into_iter()
-        .filter_map(|(key, value)| if value == 1 { Some(key) } else { None })
-        .collect();
-    let boundary_xz = faces_xz
-        .into_iter()
-        .filter_map(|(key, value)| if value == 1 { Some(key) } else { None })
-        .collect();
-    let boundary_xy = faces_xy
-        .into_iter()
-        .filter_map(|(key, value)| if value == 1 { Some(key) } else { None })
-        .collect();
-    (boundary_yz, boundary_xz, boundary_xy)
+    // Each face can belong to at most two cubes as long as the cubes are unique.
+    assert!(faces_yz.iter().all(|&count| count <= 2));
+    assert!(faces_xz.iter().all(|&count| count <= 2));
+    assert!(faces_xy.iter().all(|&count| count <= 2));
+
+    // Count the number of faces that occur exactly once.
+    let boundary_yz = faces_yz.iter().filter(|&&count| count == 1).count();
+    let boundary_xz = faces_xz.iter().filter(|&&count| count == 1).count();
+    let boundary_xy = faces_xy.iter().filter(|&&count| count == 1).count();
+    boundary_yz + boundary_xz + boundary_xy
 }
 
-pub fn solve_part1<I>(cubes: I) -> usize
-where
-    I: IntoIterator<Item = Cube<u8>>,
-{
-    let (faces_yz, faces_xz, faces_xy) = boundary_faces(cubes);
-    faces_yz.len() + faces_xz.len() + faces_xy.len()
+pub fn solve_part1(cubes: &[Cube<usize>]) -> usize {
+    count_boundary_faces(cubes)
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
@@ -134,30 +129,26 @@ enum CubeKind {
     Interior,
 }
 
-fn mark_cubes<I>(lava: I) -> Array3<CubeKind>
-where
-    I: IntoIterator<Item = Cube<u8>>,
-{
-    let lava: HashSet<_> = lava.into_iter().collect();
-    let xlimit = 2 + lava.iter().map(|cube| cube.x).max().unwrap_or(0) as usize;
-    let ylimit = 2 + lava.iter().map(|cube| cube.y).max().unwrap_or(0) as usize;
-    let zlimit = 2 + lava.iter().map(|cube| cube.z).max().unwrap_or(0) as usize;
+/// Labels each element of the array bounding the cubelets according to its component class.
+fn label_cube_kinds(lava: &[Cube<usize>]) -> Array3<CubeKind> {
+    let xlimit = 2 + lava.iter().map(|cube| cube.x).max().unwrap_or(0);
+    let ylimit = 2 + lava.iter().map(|cube| cube.y).max().unwrap_or(0);
+    let zlimit = 2 + lava.iter().map(|cube| cube.z).max().unwrap_or(0);
 
     let mut array: Array3<Option<CubeKind>> = Array3::default((xlimit, ylimit, zlimit));
 
     // Mark the lava cubes.
-    for cube in &lava {
-        let x = cube.x as usize;
-        let y = cube.y as usize;
-        let z = cube.z as usize;
-        array[[x, y, z]] = Some(CubeKind::Lava);
+    for cube in lava.iter() {
+        array[[cube.x, cube.y, cube.z]] = Some(CubeKind::Lava);
     }
 
-    // Mark the outermost exterior cells.
-    initialize_boundary(&mut array.view_mut());
+    // Identify the exterior cubes by initializing the boundary of a box which completely encloses
+    // lava cubes, and the iteratively extending the exterior label to neighbors of exterior cells.
+    let mut view = array.view_mut();
+    initialize_exterior(&mut view);
+    while update_exterior(&mut view) > 0 {}
 
-    while update_exterior(&mut array.view_mut()) > 0 {}
-
+    // Any cell not marked at this point is necessarily an interior cell.
     let shape = array.raw_dim();
     let data = array
         .into_iter()
@@ -166,142 +157,77 @@ where
     Array3::from_shape_vec(shape, data).expect("the data can fit the shape")
 }
 
-fn update_exterior(array: &mut ArrayViewMut3<Option<CubeKind>>) -> usize {
-    let shape = array.shape();
-    let xlimit = shape[0];
-    let ylimit = shape[1];
-    let zlimit = shape[2];
+/// Marks the first and last planes along each axis of the array as [`CubeKind::Exterior`].
+fn initialize_exterior(array: &mut ArrayViewMut3<Option<CubeKind>>) {
+    let (mut first, mut last) = array.multi_slice_mut((s![0, .., ..], s![-1, .., ..]));
+    first.fill(Some(CubeKind::Exterior));
+    last.fill(Some(CubeKind::Exterior));
 
+    let (mut first, mut last) = array.multi_slice_mut((s![.., 0, ..], s![.., -1, ..]));
+    first.fill(Some(CubeKind::Exterior));
+    last.fill(Some(CubeKind::Exterior));
+
+    let (mut first, mut last) = array.multi_slice_mut((s![.., .., 0], s![.., .., -1]));
+    first.fill(Some(CubeKind::Exterior));
+    last.fill(Some(CubeKind::Exterior));
+}
+
+/// Marks array elements adjacent to an exterior element along any axis as also exterior.
+fn update_exterior(array: &mut ArrayViewMut3<Option<CubeKind>>) -> usize {
     let mut updated = 0;
-    for x in 1..xlimit {
-        for y in 0..ylimit {
-            for z in 0..zlimit {
-                if array[[x, y, z]].is_none()
-                    && matches!(array[[x - 1, y, z]], Some(CubeKind::Exterior))
-                {
-                    array[[x, y, z]] = Some(CubeKind::Exterior);
-                    updated += 1;
-                }
-            }
-        }
-    }
-    for x in (0..xlimit - 1).rev() {
-        for y in 0..ylimit {
-            for z in 0..zlimit {
-                if array[[x, y, z]].is_none()
-                    && matches!(array[[x + 1, y, z]], Some(CubeKind::Exterior))
-                {
-                    array[[x, y, z]] = Some(CubeKind::Exterior);
-                    updated += 1;
-                }
-            }
-        }
-    }
-    for y in 1..ylimit {
-        for x in 0..ylimit {
-            for z in 0..zlimit {
-                if array[[x, y, z]].is_none()
-                    && matches!(array[[x, y - 1, z]], Some(CubeKind::Exterior))
-                {
-                    array[[x, y, z]] = Some(CubeKind::Exterior);
-                    updated += 1;
-                }
-            }
-        }
-    }
-    for y in (0..ylimit - 1).rev() {
-        for x in 0..xlimit {
-            for z in 0..zlimit {
-                if array[[x, y, z]].is_none()
-                    && matches!(array[[x, y + 1, z]], Some(CubeKind::Exterior))
-                {
-                    array[[x, y, z]] = Some(CubeKind::Exterior);
-                    updated += 1;
-                }
-            }
-        }
-    }
-    for z in 1..zlimit {
-        for x in 0..xlimit {
-            for y in 0..ylimit {
-                if array[[x, y, z]].is_none()
-                    && matches!(array[[x, y, z - 1]], Some(CubeKind::Exterior))
-                {
-                    array[[x, y, z]] = Some(CubeKind::Exterior);
-                    updated += 1;
-                }
-            }
-        }
-    }
-    for z in (0..zlimit - 1).rev() {
-        for x in 0..xlimit {
-            for y in 0..ylimit {
-                if array[[x, y, z]].is_none()
-                    && matches!(array[[x, y, z + 1]], Some(CubeKind::Exterior))
-                {
-                    array[[x, y, z]] = Some(CubeKind::Exterior);
-                    updated += 1;
-                }
-            }
-        }
-    }
+
+    // For each of the axes of the array, stride along each lane of that axis both forwards and
+    // backwards propogating [`CellKind::Exterior`] labels from a cell to its successor in the
+    // direction of stride.
+    array.lanes_mut(Axis(0)).into_iter().for_each(|mut lane| {
+        updated += update_lane(lane.view_mut());
+        updated += update_lane(lane.slice_mut(s![..; -1]));
+    });
+    array.lanes_mut(Axis(1)).into_iter().for_each(|mut lane| {
+        updated += update_lane(lane.view_mut());
+        updated += update_lane(lane.slice_mut(s![..; -1]));
+    });
+    array.lanes_mut(Axis(2)).into_iter().for_each(|mut lane| {
+        updated += update_lane(lane.view_mut());
+        updated += update_lane(lane.slice_mut(s![..; -1]));
+    });
     updated
 }
 
-fn initialize_boundary(array: &mut ArrayViewMut3<Option<CubeKind>>) {
-    let shape = array.shape();
-    let xlimit = shape[0];
-    let ylimit = shape[1];
-    let zlimit = shape[2];
-
-    // Mark the boundary
-    // yz planes
-    for y in 0..ylimit {
-        for z in 0..zlimit {
-            array[[0, y, z]] = Some(CubeKind::Exterior);
-            array[[xlimit - 1, y, z]] = Some(CubeKind::Exterior);
+/// Extends exterior cells along a 1-D lane of the array.
+///
+/// If a cell's predecessor in the direction of stride is an exterior cell, and the current cell is
+/// unknown (`None`), marks the current cell as also exterior.
+fn update_lane(mut array: ArrayViewMut1<Option<CubeKind>>) -> usize {
+    let mut updated = 0;
+    // Use `.cell_view()` to get interior mutability since `.windows()` takes only a shared
+    // reference.
+    array.cell_view().windows([2]).into_iter().for_each(|w| {
+        if w[1].get().is_none() && matches!(w[0].get(), Some(CubeKind::Exterior)) {
+            w[1].set(Some(CubeKind::Exterior));
+            updated += 1;
         }
-    }
-
-    // xz planes
-    for x in 0..xlimit {
-        for z in 0..zlimit {
-            array[[x, 0, z]] = Some(CubeKind::Exterior);
-            array[[x, ylimit - 1, z]] = Some(CubeKind::Exterior);
-        }
-    }
-
-    // xy planes
-    for x in 0..xlimit {
-        for y in 0..ylimit {
-            array[[x, y, 0]] = Some(CubeKind::Exterior);
-            array[[x, y, zlimit - 1]] = Some(CubeKind::Exterior);
-        }
-    }
+    });
+    updated
 }
 
-pub fn solve_part2<I>(cubes: I) -> usize
-where
-    I: IntoIterator<Item = Cube<u8>>,
-{
-    let cubes: Vec<_> = cubes.into_iter().collect();
-    let array = mark_cubes(cubes.clone());
-    let mut interior = Vec::new();
-    let shape = array.shape();
-    let xlimit = shape[0];
-    let ylimit = shape[1];
-    let zlimit = shape[2];
-    for x in 0..xlimit {
-        for y in 0..ylimit {
-            for z in 0..zlimit {
-                if matches!(array[[x, y, z]], CubeKind::Interior) {
-                    interior.push(Cube::new(x as u8, y as u8, z as u8));
-                }
+pub fn solve_part2(lava: &[Cube<usize>]) -> usize {
+    // 1. Find the interior cubes.
+    let labels = label_cube_kinds(lava);
+    let interior: Vec<_> = labels
+        .indexed_iter()
+        .filter_map(|(dim, elt)| {
+            if matches!(elt, CubeKind::Interior) {
+                Some(Cube::new(dim.0, dim.1, dim.2))
+            } else {
+                None
             }
-        }
-    }
-    let boundary_faces = solve_part1(cubes);
-    let interior_boundary_faces = solve_part1(interior);
+        })
+        .collect();
+    // 2. Count the boundary of the lava cubes, then subtract the size of the boundary of the
+    //    interior cubes.
+    let boundary_faces = solve_part1(lava);
+    let interior_boundary_faces = solve_part1(&interior);
     boundary_faces - interior_boundary_faces
 }
 
@@ -315,7 +241,7 @@ mod tests {
     fn test_part1_example() -> Result<()> {
         let input = fs::read_to_string("data/example")?;
         let cubes = parse_input(&input)?;
-        let surface_area = solve_part1(cubes);
+        let surface_area = solve_part1(&cubes);
         assert_eq!(surface_area, 64);
         Ok(())
     }
@@ -324,7 +250,7 @@ mod tests {
     fn test_part1_input() -> Result<()> {
         let input = fs::read_to_string("data/input")?;
         let cubes = parse_input(&input)?;
-        let surface_area = solve_part1(cubes);
+        let surface_area = solve_part1(&cubes);
         assert_eq!(surface_area, 4242);
         Ok(())
     }
@@ -333,7 +259,7 @@ mod tests {
     fn test_part2_example() -> Result<()> {
         let input = fs::read_to_string("data/example")?;
         let cubes = parse_input(&input)?;
-        let surface_area = solve_part2(cubes);
+        let surface_area = solve_part2(&cubes);
         assert_eq!(surface_area, 58);
         Ok(())
     }
@@ -342,7 +268,7 @@ mod tests {
     fn test_part2_input() -> Result<()> {
         let input = fs::read_to_string("data/input")?;
         let cubes = parse_input(&input)?;
-        let surface_area = solve_part2(cubes);
+        let surface_area = solve_part2(&cubes);
         assert_eq!(surface_area, 2428);
         Ok(())
     }
--- day18/src/main.rs
+++ day18/src/main.rs
@@ -6,11 +6,11 @@ use day18::{parse_input, solve_part1, solve_part2, Cube, Result};
 
 fn main() -> Result<()> {
     let input = io::read_to_string(io::stdin())?;
-    let cubes: Vec<Cube<u8>> = parse_input(&input)?;
-    let surface_area = solve_part1(cubes.clone());
+    let cubes: Vec<Cube<usize>> = parse_input(&input)?;
+    let surface_area = solve_part1(&cubes);
     println!("[Part 1] Number of faces that are not connected to another cube: {surface_area}");
 
-    let surface_area = solve_part2(cubes);
+    let surface_area = solve_part2(&cubes);
     println!("[Part 2] Exterior surface area: {surface_area}");
 
     Ok(())
