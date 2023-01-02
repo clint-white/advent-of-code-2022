aee524e55a91d7fadbdacf11a254539a4439cb7d
Clint White <104277906+clint-white@users.noreply.github.com>
2023-01-02 13:19:01 -0500

    Day 22: Add part2 solution to main program
    I had not previously added the solution to part 2 to the main program,
    instead just using the tests in the library to obtain and submit the
    answer.  We have hardcoded edge gluing rules for the example and actual
    inputs, so in order to solve part 2 in `main.rs` we have to guess which
    input we were given.  Here we use the shape of the grid to determine
    which gluing rules to use.

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -2,7 +2,7 @@
 
 use std::io;
 
-use day22::{parse_input, part1, Result};
+use day22::{parse_input, part1, part2, Result};
 
 fn main() -> Result<()> {
     let input = io::read_to_string(io::stdin())?;
@@ -10,6 +10,21 @@ fn main() -> Result<()> {
     let graph = part1::build_graph(&grid)?;
     let password = part1::solve(&graph, &instructions);
     println!("[Part 1] The final password is {password}");
+
+    let mut graph = part2::build_graph(&grid)?;
+    // We hardcode edge gluing rules for the puzzle input and the example.  Detect which we were
+    // given on stdin by the shape of the grid.
+    match grid.shape() {
+        [202, 152] => part2::glue_edges_input(&mut graph, &grid),
+        [14, 18] => part2::glue_edges_example(&mut graph, &grid),
+        _ => {
+            let error =
+                io::Error::new(io::ErrorKind::InvalidInput, "unrecognized input grid shape");
+            return Err(error.into());
+        }
+    }
+    let password = part2::solve(&graph, &instructions);
+    println!("[Part 2] The final password is {password}");
 
     Ok(())
 }
